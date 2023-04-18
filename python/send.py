01d7a4db856b2a8f848b01024afd1231290cb9be
Ruchan Yalçın <ruchany13@gmail.com>
2023-04-18 16:30:25 +0300

    Delete bot id

--- python/send.py
+++ python/send.py
@@ -2,8 +2,8 @@ import requests
 
 def telegram_bot_sendtext(bot_message):
 
-   bot_token = "1306224394:AAFUBwGCciNOiOQQQz0j9YNb5OniwEPfsHg"
-   bot_chatID = '431846525'
+   bot_token = ""
+   bot_chatID = ''
    send_text = 'https://api.telegram.org/bot' + bot_token \
       + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' \
       + bot_message
